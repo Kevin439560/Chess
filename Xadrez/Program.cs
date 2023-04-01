5b8ecb321346e803d28d48e44a887c43619c5f05
Kevin439560 <kevin.freitas560@gmail.com>
2023-04-01 16:41:32 -0300

    Classes Implementadas

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -1,9 +1,13 @@
-﻿namespace Xadrez {
+﻿using tabuleiro;
+namespace Xadrez {
 
     class Program {
 
         static void Main(string[] args) {
 
+            Tabuleiro tab = new(9, 0);
+
+            Console.ReadLine();
 
         }
     }
---
+++ Xadrez/tabuleiro/Tabuleiro.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace tabuleiro {
+    class Tabuleiro {
+
+        public int NumLinhas { get; set; }
+
+        public int NumColunas { get; set; }
+
+        private Peca[,] Pecas;
+
+        public Tabuleiro(int numLinhas, int numColunas) {
+
+            NumLinhas = numLinhas;
+
+            NumColunas = numColunas;
+
+            Pecas = new Peca[NumLinhas, NumColunas];
+
+        }
+    }
+}
